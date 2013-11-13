a99a34953edc0c8d375947932d746928254eac20
Gangster Veggies <gangsterveggies@gmail.com>
2013-11-13 19:01:55 +0000

    Update look

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def full_title(page_title)
     base_title = "Loop"
     if page_title.empty?
-      base_title
+      "#{base_title}'ONI"
     else
       "#{base_title} | #{page_title}"
     end
