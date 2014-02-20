70fcb0430d0069f54bde82d01f044a398b51f1b3
Gangster Veggies <gangsterveggies@gmail.com>
2014-02-20 01:36:03 +0000

    Fix issue #23 of sidebox

--- app/controllers/cover_pages_controller.rb
+++ app/controllers/cover_pages_controller.rb
@@ -2,6 +2,7 @@ class CoverPagesController < ApplicationController
   def home
     @posts = Post.paginate(page: params[:page], per_page: 20, :conditions => "id > 1")
     @fixed_post = Post.last
+    @contests = Contest.all(:conditions => ["end_date >= ? AND begin_date <= ?", DateTime.now(), DateTime.now() + 1.month])
   end
 
   def help
