93ccebda93c5ce041b3eb9160fe79fb1c708c171
Gangster Veggies <gangsterveggies@gmail.com>
2014-03-21 01:19:31 +0000

    Fix date form bug

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -9,8 +9,6 @@ class ContestsController < ApplicationController
 
   def new
     @contest = Contest.new
-    @contest.begin_date = DateTime.now
-    @contest.end_date = DateTime.now
   end
 
   def index
