853129bb61069f153623c4c7114b9ad9a057be7d
Gangster Veggies <gangsterveggies@gmail.com>
2014-03-21 01:49:24 +0000

    Precompile icons

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Loop
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += ['glyphicons-halflings.png', 'glyphicons-halflings-white.png']
     ActsAsTaggableOn.remove_unused_tags = true
   end
 end
