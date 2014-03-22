28ece708d2835e87a12909ee6f3d4905c96aa4e6
Gangster Veggies <gangsterveggies@gmail.com>
2014-03-22 03:43:17 +0000

    Change syntax highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,7 +16,7 @@ module ApplicationHelper
 
   def coderay(text)  
     text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do
-      CodeRay.scan($3, $2).div(:css => :class, :line_numbers => :table)
+      CodeRay.scan($3, $2).div(:css => :class)
     end
   end
 
